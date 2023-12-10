2bf466b785df5c4d0ba848ae442be87dc8d28116
Emtiasur Rahman <55427849+unmad24@users.noreply.github.com>
2023-12-10 13:11:15 +0600

    Remove all module files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "04d447f5a1604d2a"; }
+/******/ 		__webpack_require__.h = function() { return "5444c7581424efd3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
