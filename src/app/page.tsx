892fdd1077ff1f2dbfa80998988d63cf654d7b20
Emtiasur Rahman <55427849+emti19@users.noreply.github.com>
2025-01-01 15:23:22 +0600

    minor changes for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -291,7 +291,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software Engineer, Researcher, and Explorer.
+            Software Engineer, AI Researcher, and Explorer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Emtias, a Software Engineer and Researcher based in Dhaka, Bangladesh.
